71f63ef39b171da04cd6f792bb89afed248ae83b
jeffudacity <jeff.donovan@udacity.com>
2015-11-04 17:48:45 -0800

    Final code from completion of webcast.

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__name__, template_folder='jeffs_templates')
+
+from flask import render_template
 
 
 @app.route('/')
 def main_page():
-    return 'Main Page'
+    return render_template('base.html')
 
 
 @app.route('/order')
 def order_page():
-    return 'Order Page'
+    our_fruits = ['Apple', 'Banana', 'Orange', 'Grape', 'Lemon']
+    return render_template('order.html', foods=our_fruits)
+
+
+@app.route('/about')
+def about_page():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.debug = True
